cdfba9e77ab26aebbe201d59e0892d26fdaa990e
nsteenla <nicolas.steenlant@ugent.be>
2023-01-25 09:21:53 +0100

    LogRequests uses X-Request-ID again

--- zaphttp.go
+++ zaphttp.go
@@ -28,7 +28,7 @@ func Logger(c context.Context) *zap.Logger {
 	return nil
 }
 
-// SetLogger is a middleware to set a zap logger in the request Context. If the
+// SetLogger is a middleware to set a zap logger in the request Context. If a
 // X-Request-ID header is present, the logger will be set to a request scoped
 // logger with a requestID field.
 func SetLogger(logger *zap.Logger) func(http.Handler) http.Handler {
@@ -46,16 +46,24 @@ func SetLogger(logger *zap.Logger) func(http.Handler) http.Handler {
 
 // LogRequests is a middleware to log requests to a zap logger.
 // The message will be set to "request" and the following request fields will be logged:
+//   - requestID (string)
 //   - method (string)
 //   - url (string)
 //   - status (int)
 //   - latency (time.Duration)
 //   - bytes (int64)
 //
+// The requestID field will only be set if a X-Request-ID header is present.
+//
 // The log level will be set to error if status >= 500, info otherwise.
 func LogRequests(logger *zap.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			l := logger
+			if requestID := r.Header.Get("X-Request-ID"); requestID != "" {
+				l = l.With(zap.String("requestID", requestID))
+			}
+
 			m := httpsnoop.CaptureMetrics(next, w, r)
 
 			lvl := zapcore.InfoLevel
@@ -63,7 +71,7 @@ func LogRequests(logger *zap.Logger) func(http.Handler) http.Handler {
 				lvl = zapcore.ErrorLevel
 			}
 
-			logger.Log(lvl, "request",
+			l.Log(lvl, "request",
 				zap.String("method", r.Method),
 				zap.String("url", r.URL.String()),
 				zap.Int("status", m.Code),
